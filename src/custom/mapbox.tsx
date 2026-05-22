0db0d73fdcc8c00a42aca0a28179e27b162d1235
Andrew Plotkin <zarf@ZarfLent.local>
2026-05-21 23:24:26 -0400

    Get all the controls in place.

--- src/custom/mapbox.tsx
+++ src/custom/mapbox.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 import { GameMap } from '../visi/map';
+import { ObjListSorter } from './cwidgets';
 
 export function GameMapBox()
 {
+    const [ followKey, setFollowKey ] = useState(0);
+    const [ showTransit, setShowTransit ] = useState(false);
+
+    function evhan_transit_change() {
+        setShowTransit(!showTransit);
+    }
+
     return (
         <div className="MapBox">
             <div className="MapTabBar">
-                Hello there.
+                <ObjListSorter followKey={ followKey } setFollowKey={ setFollowKey } />
+                <div>
+                    <input id="showtransit" type="checkbox" name="showtransit" onChange={ evhan_transit_change } />
+                    <label htmlFor="showtransit">Show Transit Layer</label>
+                </div>
             </div>
             <div className="ScrollXYContent">
             </div>
