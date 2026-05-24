15b8f023db668523769b3281e2c1337f3a83bd53
Andrew Plotkin <zarf@ZarfLent.local>
2026-05-24 10:37:20 -0400

    Fix link.

--- src/custom/about.tsx
+++ src/custom/about.tsx
@@ -182,7 +182,7 @@ export function AboutPage()
                 <h2>Sources and acknowledgements</h2>
                 <p>
                     The game&#x2019;s source code was first{' '}
-                    <ExtWebLink url={ 'https://github.com/historicalsource/deadline' } text={ 'publicly released' } />
+                    <ExtWebLink url={ 'https://github.com/historicalsource/suspended' } text={ 'publicly released' } />
                     {' '}by Jason Scott in April 2019.
                     I then combed through all known versions and posted my{' '}
                     <ExtWebLink url={ 'https://eblong.com/infocom/' } text={ 'Obsessively Complete Infocom Catalog' } />,
