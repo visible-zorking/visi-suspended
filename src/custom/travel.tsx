8cb23beb2f5e29ac8cdc8025fd19a9e54c36fea3
Andrew Plotkin <zarf@ZarfLent.local>
2026-05-26 22:07:52 -0400

    The schedule page.

---
+++ src/custom/travel.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { useState, useContext } from 'react';
+
+import { ZilSourceLoc } from '../visi/main';
+import { ReactCtx } from '../visi/context';
+
+import { signed_zvalue, unpack_address } from '../visi/gametypes';
+import { gamedat_object_ids, gamedat_routine_addrs, gamedat_property_nums } from '../visi/gamedat';
+
+import { robot_names } from './info';
+import { SpecificSuspended } from './modgame';
+
+
+// This doesn't require a context, turns out.
+function evhan_click_id(ev: React.MouseEvent<HTMLAnchorElement, MouseEvent>, id: string) {
+    ev.preventDefault();
+    let dat: ZilSourceLoc = { id: id, commentary: true };
+    window.dispatchEvent(new CustomEvent('zil-source-location', { detail:dat }));
+}
+
+function GoalTable()
+{
+    let rctx = useContext(ReactCtx);
+    let zstate = rctx.zstate;
+    
+    let specifics = zstate.specifics as SpecificSuspended;
+    
+    let rowls = [];
+    for (let char=1; char<=7; char++) {
+        rowls.push(
+            <GoalTableRow key={ char } char={ char } row={ specifics.goaltables[char] } />
+        );
+    }
+    
+    return (
+        <table className="GoalTable">
+            <tbody>
+                <tr>
+                    <th>person</th>
+                    <th>final</th>
+                    <th>station</th>
+                    <th>inter</th>
+                    <th>dir</th>
+                    <th>run</th>
+                </tr>
+                { rowls }
+            </tbody>
+        </table>
+    );
+}
+
+function GoalTableRow({ char,  row }: { char:number, row:number[] })
+{
+    let rctx = useContext(ReactCtx);
+
+    let obj0 = gamedat_object_ids.get(row[0]);
+    let obj1 = gamedat_object_ids.get(row[1]);
+    let obj2 = gamedat_object_ids.get(row[2]);
+    
+    return (
+        <tr>
+            <td>{ robot_names[char] }</td>
+            <td>
+                {
+                    obj0 ?
+                    <a className="Src_Id" href="#" onClick={ (ev) => evhan_click_id(ev, 'OBJ:'+obj0.name) }>{ obj0.name }</a>
+                    : '\u2014'
+                }
+            </td>
+            <td>
+                {
+                    obj1 ?
+                    <a className="Src_Id" href="#" onClick={ (ev) => evhan_click_id(ev, 'OBJ:'+obj1.name) }>{ obj1.name }</a>
+                    : '\u2014'
+                }
+            </td>
+            <td>
+                {
+                    obj2 ?
+                    <a className="Src_Id" href="#" onClick={ (ev) => evhan_click_id(ev, 'OBJ:'+obj2.name) }>{ obj2.name }</a>
+                    : '\u2014'
+                }
+            </td>
+            <td>
+                &#x2014;
+            </td>
+            <td>
+                { (row[4] ?
+                   <span className="TimerActive">&#x2611;</span> :
+                   <span className="TimerInactive">&#x2610;</span>) }
+            </td>
+        </tr>
+    )
+}
