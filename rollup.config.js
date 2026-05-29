91338a5bfa012f8487ae3e36e47652a7d14cb7a4
Andrew Plotkin <zarf@ZarfLent.local>
2026-05-28 23:43:27 -0400

    Date, and release mode.

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,8 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import terser from '@rollup/plugin-terser';
 
-const datestr = 'May 26, 2026';
-const release = false;
+const datestr = 'May 28, 2026';
+const release = true;
 
 // See: https://rollupjs.org/configuration-options/
 
