28edf7c5da23ff86d8eb0436043c46f53080cad7
Andrew Plotkin <zarf@ZarfLent.local>
2026-06-21 21:46:43 -0400

    Add a line to the ABOUT page.

--- src/custom/about.tsx
+++ src/custom/about.tsx
@@ -103,6 +103,8 @@ export function AboutPage()
                     shows all the game&#x2019;s global variables.
                     {' '}<a className="Internal" href="#" onClick={ (ev)=>evhan_click_tab(ev, 'timers') }>Timers</a>{' '}
                     shows the table of timed events.
+                    {' '}<a className="Internal" href="#" onClick={ (ev)=>evhan_click_tab(ev, 'travel') }>Travel</a>{' '}
+                    shows the table that determines where robots are heading.
                     {' '}<a className="Internal" href="#" onClick={ (ev)=>evhan_click_tab(ev, 'grammar') }>Grammar</a>{' '}
                     shows the parse table.
                     {' '}<a className="Internal" href="#" onClick={ (ev)=>evhan_click_tab(ev, 'map') }>Map</a>{' '}
